97403f3b9d0f02276f1b21f83146f27d1ba3a2b1
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2023-12-02 10:52:27 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+a = prompt("Enter any word to identify palindrome or not")
+
+var len = a.length
+
+for(i=0; i < len/2 ; i++){
+   
+}
+ 
+if(a[i]==a[len-1-i]){
+
+    alert(a + " is palindrome")
+}
+else
+{
+   alert(a + " is not palindrome ")
+}
